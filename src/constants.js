7b42e49060fe96491e33315c76f5b665d17ddb37
iladesio <desioilary@gmail.com>
2024-01-24 12:44:54 +0100

    DeployContracts and constants, other files

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+module.exports = Object.freeze({
+    ADMIN_ACCOUNT : '0xbB17bf7a6c0432Ec25E3ea0D53180216b0bb83B8',
+    CONTRACTS : [
+        { name: 'SimpleTransaction'} //by name you can access to the file json with concat the name
+       // { name: 'ContractName2'}
+      ],
+    CONTRACTS_JSON : '/Users/ilariadesio/Desktop/Computerscience/PuzzleBlock-BlockchainProject/src/contracts/contracts.json'
+});
+
--- src/index.js
+++ src/index.js
@@ -1,57 +1,45 @@
 const fs = require('fs'); // Rimuovere 'node:' per evitare problemi di compatibilità
 const {Web3} = require('web3'); // Importare Web3 correttamente
-
 const path = require('path');
-const filePath = path.resolve(__dirname, './contracts/SimpleTransaction.txt');
+var constants = require('./constants');
 
 // Connessione al provider locale di Ethereum
 web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
 
-// Carica l'ABI e il bytecode del contratto
-const ABI = require('./contracts/SimpleTransaction.json');
-const bytecode = fs.readFileSync(filePath, 'utf8');
+async function deployContracts() {
+    let contractsInfo = [];
+    
+    console.log("Total number of contracts to deploy:", constants.CONTRACTS.length);
 
-/*const deploy = async () => {
-    try {
-        // Ottieni l'elenco degli account dal tuo provider
-        const accounts = await web3.eth.getAccounts();
+    for (let contractData of constants.CONTRACTS) {
+        const filePath = path.resolve(__dirname, './contracts/' + contractData['name'] + '.txt');
+        // Carica l'ABI e il bytecode del contratto
+        const ABI = require('./contracts/' + contractData['name'] + '.json');
+        const bytecode = fs.readFileSync(filePath, 'utf8');
+        
+        console.log("Deploying Contract:", contractData['name']);
+        let contractInstance = new web3.eth.Contract(ABI);
+        
+        try {
+            let newContract = await contractInstance
+                .deploy({ data: bytecode })
+                .send({ from: constants.ADMIN_ACCOUNT, gas: 4700000 });
+            
+            console.log("Contract Address:", newContract.options.address);
 
-        // Crea un nuovo contratto con Web3 e l'ABI del contratto
-        const myContract = new web3.eth.Contract(ABI);
-
-        // Opzioni per il deploy: from, gas, e data (bytecode)
-        const deployOptions = {
-            from: accounts[0], // Indirizzo che deploya il contratto
-            gas: 1500000,      // Imposta un limite di gas adeguato
-            data: bytecode
-        };
-        console.log(typeof(accounts[0]))
-        // Effettua il deploy del contratto
-        const deployedContract = await myContract.deploy(deployOptions).send();
-
-        // Stampa l'indirizzo del contratto deployato
-        console.log('Contratto deployato all\'indirizzo:', deployedContract.options.address);
-    } catch (error) {
-        console.error('Errore durante il deploy:', error);
+            // Aggiungi le informazioni del contratto a contractsInfo
+            contractsInfo.push({
+                name: contractData['name'],
+                address: newContract.options.address,
+                abi: ABI,
+                bytecode: bytecode
+            });
+        } catch (error) {
+            console.error("Error deploying contract:", contractData['name'], error);
+        }
     }
-};*/
+    fs.writeFileSync(constants.CONTRACTS_JSON, JSON.stringify(contractsInfo, null, 2));
+    console.log('Contracts information has been saved to ' + constants.CONTRACTS_JSON);
+}
 
-contract = new web3.eth.Contract(ABI);
-
-web3.eth.getAccounts().then((accounts) => {
-    // Display all Ganache Accounts
-    console.log("Accounts:", accounts);
- 
-    mainAccount = accounts[0];
- 
-    // address that will deploy smart contract
-    console.log("Default Account:", mainAccount);
-    contract
-        .deploy({ data: bytecode })
-        .send({ from: mainAccount, gas: 470000 })
-        .on("receipt", (receipt) => {
- 
-            // Contract Address will be returned here
-            console.log("Contract Address:", receipt.contractAddress);
-        })
-});
+deployContracts();
