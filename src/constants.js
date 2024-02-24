3f525bfa16004d7469ebd5d53587f683b362be3d
bellogiuseppe <121182585+bellogiuseppe@users.noreply.github.com>
2024-02-24 16:58:20 +0100

    minor bug fixes

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,6 @@
 module.exports = Object.freeze({
-    ADMIN_ACCOUNT: '0xAE4B2d7E9856398ab3EC48902415DAF19de2da6E',
+    ADMIN_ACCOUNT: '0x315882EAfFCD39D76b937Fa4Cf11a41067b2e63d',
+    GANACHE_URL: 'http://127.0.0.1:7545',
     CONTRACTS : [
         { name: 'PuzzleContract'}
       ],
@@ -7,6 +8,5 @@ module.exports = Object.freeze({
     PORT: 3000,
     PINATA_GATEWAY: 'https://coral-tremendous-wasp-739.mypinata.cloud',
     PINATA_GATEWAY_2: 'https://bronze-personal-meadowlark-873.mypinata.cloud'
-
 });
 
