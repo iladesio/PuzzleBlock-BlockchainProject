53a74ad5bc88c9c34b9b554282a9577df1b3f139
iladesio <desioilary@gmail.com>
2024-02-26 20:27:19 +0100

    edits

--- src/constants.js
+++ src/constants.js
@@ -2,8 +2,8 @@ module.exports = Object.freeze({
     ADMIN_ACCOUNT: '0xbB17bf7a6c0432Ec25E3ea0D53180216b0bb83B8',
     GANACHE_URL: 'http://127.0.0.1:7545',
     CONTRACTS : [
-        { name: 'PuzzleContract'},
-        { name: 'GameAsset' }
+        { name: 'PuzzleContract'}
+        //{ name: 'GameAsset' }
       ],
     CONTRACTS_JSON : './src/contracts/contracts.json',
     PORT: 3000,
