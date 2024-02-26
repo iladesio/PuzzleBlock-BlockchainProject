41014085385b4491aa1b583dabd4e434142067cb
Federico Barreca <federico.barreca97@gmail.com>
2024-02-26 21:00:57 +0100

    added constants

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 module.exports = Object.freeze({
-    ADMIN_ACCOUNT: '0xbB17bf7a6c0432Ec25E3ea0D53180216b0bb83B8',
+    ADMIN_ACCOUNT: '0xAE4B2d7E9856398ab3EC48902415DAF19de2da6E',
     GANACHE_URL: 'http://127.0.0.1:7545',
     CONTRACTS : [
         { name: 'PuzzleContract'}
@@ -8,6 +8,7 @@ module.exports = Object.freeze({
     CONTRACTS_JSON : './src/contracts/contracts.json',
     PORT: 3000,
     PINATA_GATEWAY: 'https://coral-tremendous-wasp-739.mypinata.cloud',
-    PINATA_GATEWAY_2: 'https://bronze-personal-meadowlark-873.mypinata.cloud'
+    PINATA_GATEWAY_2: 'https://bronze-personal-meadowlark-873.mypinata.cloud',
+    MIN_SCORE: 50, MAX_SCORE:200
 });
 
