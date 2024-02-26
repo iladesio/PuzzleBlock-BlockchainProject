60699856f15cd78dd72bfeffe82010b77731b06b
iladesio <desioilary@gmail.com>
2024-02-26 20:08:15 +0100

    new implementation  for nfts and new contract

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,9 @@
 module.exports = Object.freeze({
-    ADMIN_ACCOUNT: '0x315882EAfFCD39D76b937Fa4Cf11a41067b2e63d',
+    ADMIN_ACCOUNT: '0xbB17bf7a6c0432Ec25E3ea0D53180216b0bb83B8',
     GANACHE_URL: 'http://127.0.0.1:7545',
     CONTRACTS : [
-        { name: 'PuzzleContract'}
+        { name: 'PuzzleContract'},
+        { name: 'GameAsset' }
       ],
     CONTRACTS_JSON : './src/contracts/contracts.json',
     PORT: 3000,
---
+++ src/contracts/GameAsset.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract GameAsset is ERC1155{
+    uint256 public constant SKELECAT = 1;
+    uint256 public constant COSMOSQUIT = 2;
+    uint256 public constant TECHNOTOK = 3;
+
+    uint256[] listIds = [SKELECAT,COSMOSQUIT,TECHNOTOK];
+
+    constructor() ERC1155("https://bronze-personal-meadowlark-873.mypinata.cloud/ipfs/QmUftc8LtmFfdcQrm3iNtVwHahay18V8CntcpmHeeskPJ9/?pinataGatewayToken=UFLPlgEKciyWU0K8szufd_ShjfnlY4vj5B06b7hYldAtABp9lPc1553JjfRHwtzf/{id}.json"){
+    }
+
+    function createToken (address account,uint256[] memory ids,uint256[] memory amounts,bytes memory data) public {
+        ids = listIds;
+        _mintBatch(account, ids, amounts, data);
+    }
+
+    function getIds() public view returns(uint256[] memory){
+        return listIds;
+    }
+
+    function uri(uint256 _tokenId) override public pure returns (string memory){
+        return string(
+            abi.encodePacked(
+                "https://bronze-personal-meadowlark-873.mypinata.cloud/ipfs/QmUftc8LtmFfdcQrm3iNtVwHahay18V8CntcpmHeeskPJ9/?pinataGatewayToken=UFLPlgEKciyWU0K8szufd_ShjfnlY4vj5B06b7hYldAtABp9lPc1553JjfRHwtzf/",
+                Strings.toString(_tokenId),
+                ".json"
+            )
+        );
+    }
+}
