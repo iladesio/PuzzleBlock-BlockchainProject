ce1af70268f85252456cd059a5479c72bce6586a
iladesio <desioilary@gmail.com>
2024-03-12 21:22:49 +0100

    create folder for submodule unity

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 module.exports = Object.freeze({
-    ADMIN_ACCOUNT: '0xAE4B2d7E9856398ab3EC48902415DAF19de2da6E',
+    ADMIN_ACCOUNT: '0xbB17bf7a6c0432Ec25E3ea0D53180216b0bb83B8',
     GANACHE_URL: 'http://127.0.0.1:7545',
     CONTRACTS: [
         { name: 'PuzzleContract' },
